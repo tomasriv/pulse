ee2c906d131f8ea35d1b9bde70c7a3d078ca231e
tomasriv <2015030124@upsin.edu.mx>
2018-08-11 12:26:57 -0600

    Update app.py

--- app.py
+++ app.py
@@ -24,4 +24,4 @@ def echo_socket(ws):
 		ws.send(signals)
 
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=int("5000"), debug = True)
+	app.run(host="0.0.0.0", port=int("5000"), debug = True)
